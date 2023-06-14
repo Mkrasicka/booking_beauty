f3e679b1428c2df40bb3d32353936372c3bcec64
magdalena krasicka <krasickamagdalena@gmail.com>
2023-06-14 15:45:23 +0100

    mvc - views and routes need reviewing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'appointments/new'
+  get 'appointments/update'
+  get 'appointments/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "bookings#home"
+
+  resources :appointments, only: [:new, :update, :show] do
+    resources :services, only: [:index, :create, :update, :destroy]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
